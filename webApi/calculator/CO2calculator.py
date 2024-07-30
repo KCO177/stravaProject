5901e083d7dce44b37e15477a31c5d4adb869562
KCO177 <koscelnik.p@gmail.com>
2024-07-30 15:53:40 +0200

    initial structure

--- webApi/calculator/CO2calculator.py
+++ webApi/calculator/CO2calculator.py
@@ -1,13 +1,18 @@
 class CO2_calculator:
 
-    def one_km_weigth(km):
-        # weigth [g] CO2 for 1 litr
-        #litr_nafta = 2640
-        litr_benzin = 2390
-        cena_ben = 36.7
-        avg_l = 6.5 #AVG consumption of your car for 100 km
 
-        one_km = litr_benzin*avg_l/100
+    def one_km_weigth(filteredRides):
+        km = int(filteredRides['Distance'].sum()) / 1000
+        print(km)
+        # weigth [g] CO2 for 1 litr
+        litr_nafta = 2640
+        litr_benzin = 2390
+        litr_fuel = 2500
+        cena_phm = 37.5 #TODO get from web
+        cena_ben = 36.7 #TODO get from web
+        avg_l = 6.5 #AVG consumption of your car for 100 km TODO make input
+
+        one_km = litr_fuel*avg_l/100
         total_km = round((km/100) * avg_l, 2)
         print('total consuption', total_km)
         total = round(one_km * km/1000, 2)
@@ -18,9 +23,4 @@ class CO2_calculator:
         #print('today you save', total, 'kg of CO2')
         #print('you should save', week,'kg in week')
 
-    def cal_hour(duration):
-        #from mountain bike moderate cycling
-        minute_cycling = 791/60
-        total_cal = round(duration*minute_cycling, 2)
-        print('you burn total', total_cal,'cal')
-        #print('you should burn additional', total_cal*5,'cal in one week' )
+
