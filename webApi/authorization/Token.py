2d24db5df26b6a11129feb91e7000247eb14dc49
KCO177 <koscelnik.p@gmail.com>
2024-07-31 14:23:18 +0200

    Add db REST function and access token logic

--- webApi/authorization/Token.py
+++ webApi/authorization/Token.py
@@ -10,16 +10,17 @@ class Token:
         token = self.fetch_data(clientId, "short_lived_access_token", "access_token")
         if None or self.is_expired(clientId):
             print('authorization token invalid or missing, start authorization process')
-
+            newToken = self.set_access_token(clientId)
+            return newToken
         else:
             return token
 
-    def save_access_token(self, clientId):
+    def set_access_token(self, clientId):
         authorization_instance = Authorization()
         token, expiration_timestamp = authorization_instance.getAccessToken(clientId)
         self.insert_data(clientId, "short_lived_access_token", "access_token", token)
         self.insert_data(clientId, "short_lived_access_token", "expiration", expiration_timestamp)
-
+        return token
     def connect_db(self):
         try:
             conn = self._create_connection()
@@ -87,10 +88,13 @@ class Token:
 
     def is_expired(self, clientId):
         #True is it is expired
-        expiration_date = self.convert_unix_timestamp(self.fetch_data(clientId, "short_lived_acces_token", "expire_at"))
-        expiration_timestamp = self.convert_unix_timestamp(expiration_date)
-        current_timestamp = int(datetime.datetime.now().timestamp())
-        return current_timestamp > expiration_timestamp
+        expiration_date_from_db = self.fetch_data(clientId, "short_lived_acces_token", "expire_at")
+        if expiration_date_from_db == None: pass #TODO call authorization
+        else:
+            expiration_date = self.convert_unix_timestamp(expiration_date_from_db)
+            expiration_timestamp = self.convert_unix_timestamp(expiration_date)
+            current_timestamp = int(datetime.datetime.now().timestamp())
+            return current_timestamp > expiration_timestamp
 
     def convert_unix_timestamp(self, timestamp):
         dt = datetime.datetime.fromtimestamp(timestamp)
